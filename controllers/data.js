c56a2ad84fd9463ef9a2363869e9f3653c985d38
VampireRabbit <1662025808@qq.com>
2024-02-17 23:31:20 -0500

    Update data.js

--- controllers/data.js
+++ controllers/data.js
@@ -4,6 +4,18 @@ import model from '../model.js'
 const md = model('data')
 srpc.data = {}
 
-srpc.data.get = async filter => {
+srpc.data.find = async filter => {
   return await md.find(filter)
+} 
+
+srpc.data.put = async (_id, data) => {
+  return await md.put({ _id }, data)
+}
+
+srpc.data.update = async (_id, data) => {
+  return await md.update({ _id }, data)
+}
+
+srpc.data.del = async filter => {
+  return await md.del(filter)
 } 
