82956c1146cf7b1c9fb4d7332619799159fac64e
dinghaoran <1006433636@qq.com>
2024-02-27 23:38:58 +0800

    Update home page

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,12 +1,6 @@
-<script>
-  import SmileIcon from '$lib/components/SmileIcon.svelte'
-  let cot = $state(0)
-
-  function inc () {
-    cot++
-  }
-</script>
-
-<SmileIcon></SmileIcon>
-<p>cot: {cot}</p>
-<button onclick={inc} class="p-2 rounded">Click Me</button>
+<div class="bg-gray-100 min-h-screen flex items-center justify-center">
+  <div class="max-w-screen-lg mx-auto text-center">
+    <h1 class="text-4xl font-extrabold tracking-tight text-black sm:text-5xl md:text-6xl">AICE</h1>
+    <p class="mt-3 text-xl text-gray-500 sm:mt-4">可视化AI数据库与动态交互Flow设计系统</p>
+  </div>
+</div>
