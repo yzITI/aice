123b98bc1a66243ebb944a4cbd64f31e6e3388ed
Phantomlsh <root@li.yzzx.tech>
2024-02-28 19:44:26 +0000

    Add srpc

---
+++ web/src/lib/state.svelte.js
@@ -0,0 +1,3 @@
+const state = $state({})
+
+export default state
--- web/src/routes/flow/+page.svelte
+++ web/src/routes/flow/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+  import srpc from '$lib/srpc.js'
+  async function getList () {
+    const res = await srpc.flow.getList()
+    console.log(res)
+  }
+  getList()
+</script>
+
 <div class="p-6">
   <h1 class="text-4xl font-bold">Flow</h1>
   <p>This page is under construction</p>
