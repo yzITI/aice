650a3b1210769f4b71542075cc1a9a0a9570cb51
VampireRabbit <1662025808@qq.com>
2024-03-15 18:35:30 -0400

    Update +page.svelte

--- web/src/routes/flow/[_id]/+page.svelte
+++ web/src/routes/flow/[_id]/+page.svelte
@@ -4,10 +4,12 @@
   import S from '$lib/S.svelte'
   import swal from 'sweetalert2'
   import moment from 'moment'
+  import { AIcon } from 'ace.svelte'
+  import { mdiTrashCanOutline, mdiCodeTags } from '@mdi/js'
   const parseTime = t => moment(t).format('YYYY-MM-DD HH:mm:ss')
 
   let { data } = $props()
-  let flow = $state({})
+  let flow = $state({}), step = $state({})
   let stepGraph = $state([])
 
   async function fetch () {
@@ -20,16 +22,31 @@
     console.log(stepGraph)
   }
   fetch()
+
+  async function choose (i, j) {
+    step = flow.steps[stepGraph[i][j]]
+  }
 </script>
 
-<div class="p-6 min-h-screen">
-  <h1 class="text-2xl font-bold">{flow.name}</h1>
+<div class="p-6 min-h-screen flex flex-col bg-gray-100">
+  <h1 class="text-2xl font-bold my-2">{flow.name}</h1>
   <div>
-    {#each stepGraph as chain}
-      {#each chain as stepid}
-        <div>{stepid}</div>
+    <div class="w-1/4">
+      {#each stepGraph as chain, i}
+        {#each chain as stepid, j}
+          <div class="bg-white flex my-1 p-2 rounded justify-between items-center shadow cursor-pointer all-transition hover:shadow-md">
+            <div>{stepid}</div>
+            <button>
+              <AIcon path={mdiTrashCanOutline} size="1.5rem" class="text-red-600"></AIcon>
+            </button>
+          </div>
+        {/each}
+      <hr class="my-2">
       {/each}
-    {/each}
+    </div>
+    <div>
+  
+    </div>
   </div>
-  <p>id: {data._id}</p>
+  <!-- <p>id: {data._id}</p> -->
 </div>
